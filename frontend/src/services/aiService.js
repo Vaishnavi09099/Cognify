41be6083a8f21188994c15c441dce10387cc4376
Vaishnavi <Vaishnavi09.099@gmail.com>
2026-03-06 02:54:35 +0530

    Added aiService.js

---
+++ frontend/src/services/aiService.js
@@ -0,0 +1,81 @@
+import { API_PATHS } from "../utils/apiPaths.js";
+import axiosInstance from "../utils/axiosInstance.js";
+
+const genrateFlashcards = async (documentId , options)=>{
+    try{
+        const response = await axiosInstance.post(API_PATHS.AI.GENERATE_FLASHCARDS,{
+           documentId,...options
+        })
+
+        return response.data;
+
+    }catch(err){
+        throw new Error("Failed to generate flashcards");
+    }
+}
+const generateQuiz = async (documentId , options)=>{
+    try{
+        const response = await axiosInstance.post(API_PATHS.AI.GENERATE_QUIZ,{
+           documentId,...options
+        })
+
+        return response.data;
+
+    }catch(err){
+        throw new Error("Failed to generate QUIZ");
+    }
+}
+
+const generateSummary = async(documentId)=>{
+    try{
+        const response = await axiosInstance.post(API_PATHS.AI.GENERATE_SUMMARY,{
+           documentId
+        })
+        return response.data;
+
+    }catch(err){
+        throw new Error("Failed to generate summary!")
+
+    }
+}
+
+const chat = async(documentId,message)=>{
+    try{
+        const response = await axiosInstance.get(API_PATHS.AI.CHAT,{
+            documentId,question:message
+        });
+        return response.data;
+
+    }catch(err){
+          throw new Error("Char response failed!")
+
+    }
+}
+const explainConcept = async(documentId,concept)=>{
+    try{
+        const response = await axiosInstance.get(API_PATHS.AI.EXPLAIN_CONCEPT,{documentId,concept});
+        return response.data;
+
+    }catch(err){
+          throw new Error("Failed to explain concept!")
+
+    }
+}
+const getChatHistory = async(documentId)=>{
+    try{
+        const response = await axiosInstance.get(API_PATHS.AI.GET_CHAT_HISTORY(documentId));
+        return response.data;
+
+    }catch(err){
+          throw new Error("Failed to fetch chat history!")
+
+    }
+}
+
+
+
+const authService = {
+    genrateFlashcards,getChatHistory,explainConcept,updateProfile,chat,generateSummary,generateQuiz 
+}
+
+export default authService;
